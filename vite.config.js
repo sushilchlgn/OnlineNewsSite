ad9a0ed80fceaa3695ef6252a754c1e83eb2abf8
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-06 15:32:30 +0545

    home page is created

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/style.css',
                 'resources/js/app.js',
+                'resources/js/main.js',
+
             ],
             refresh: true,
         }),

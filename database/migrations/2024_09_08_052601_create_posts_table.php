7244af48c34d7398a2c774f3e8503e5ccd880f3f
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-10 16:57:51 +0545

    removed comment

--- database/migrations/2024_09_08_052601_create_posts_table.php
+++ database/migrations/2024_09_08_052601_create_posts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            // $table->string('category');
             $table->unsignedBigInteger('category_id');
             $table->timestamps();
 

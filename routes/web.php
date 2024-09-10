21cce1214e3993eacfe4d0bc08432c87d2a16e10
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-10 17:25:26 +0545

    posts to auth

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', [HomePageController::class, 'index']);
 
 Route::resource('categories', CategoryController::class);
 
-Route::resource('posts', PostController::class);
+
 
 
 // Route::get('/post/{id}',[SingalPageController::class,'show'])->name('posts.page');
@@ -35,6 +35,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('posts', PostController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

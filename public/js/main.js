e190fe3eeb8a92d4b21231bfd723978c95a1374e
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-11 17:06:40 +0545

    toggle replay

--- public/js/main.js
+++ public/js/main.js
@@ -161,3 +161,12 @@
     
 })(jQuery);
 
+
+function showReplyForm(commentId) {
+    var replyForm = document.getElementById('reply-form-' + commentId);
+    if (replyForm.style.display === 'none') {
+        replyForm.style.display = 'block';
+    } else {
+        replyForm.style.display = 'none';
+    }
+}

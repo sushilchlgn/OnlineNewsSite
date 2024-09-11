93f2ca803d5c75f7a85b0e05cf95ac43df11c17a
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-11 13:24:37 +0545

    show comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function index(){
+        $comments = Comment::all();
+        return view("admin.comments.index",compact("comments"));
+    }
     public function store(Request $request, Post $post){
         $request->validate([
             'body'=>'required',
         ]);
 
-        Post::create([
+        Comment::create([
             'post_id'=> $post->id,
             'user_id'=> auth()->id(),
             'body'=> $request->body,

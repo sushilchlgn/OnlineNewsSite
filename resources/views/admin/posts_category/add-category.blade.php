f39f86dc61a2209581f0bf1677ac65cf5304d516
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-13 15:26:04 +0545

    same page for update and create category

--- resources/views/admin/posts_category/add-category.blade.php
+++ resources/views/admin/posts_category/add-category.blade.php
@@ -3,7 +3,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Add Categorys') }}
+            {{ isset($category) ? __('Update Category') : __('Add Category') }}
         </h2>
     </x-slot>
 
@@ -11,27 +11,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white relative overflow-x-auto shadow sm:rounded-lg">
                 <div class="w-full max-w-xs">
-                    <form class="bg-white rounded px-8 pt-6 pb-8 mb-4" action="{{route('category.store')}}" method="POST" >
+                    <form class="bg-white rounded px-8 pt-6 pb-8 mb-4" 
+                          action="{{ isset($category) ? route('category.update', $category->id) : route('category.store') }}" 
+                          method="POST">
                         @csrf
-                        @method('POST')
+                        @if (isset($category))
+                            @method('PUT') <!-- Use PUT method for updates -->
+                        @endif
+
                         <div class="mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="category">
-                                Cateogry
+                                Category
                             </label>
                             <input
                                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="category" type="text" name="name" placeholder="Enter Category">
+                                id="category" 
+                                type="text" 
+                                name="name" 
+                                placeholder="Enter Category"
+                                value="{{ isset($category) ? $category->name : '' }}" 
+                                required>
                         </div>
+
                         <div class="flex items-center justify-between">
                             <button
                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                 type="submit">
-                                Add Cateogry
+                                {{ isset($category) ? 'Update Category' : 'Add Category' }}
                             </button>
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>

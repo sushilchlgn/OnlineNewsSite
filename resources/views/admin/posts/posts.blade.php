3718d1ea7c6b3318c4713b87521fde66898a623b
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-13 14:19:34 +0545

    added route for edit post

--- resources/views/admin/posts/posts.blade.php
+++ resources/views/admin/posts/posts.blade.php
@@ -24,10 +24,10 @@
             @endif
 
             <div class="p-4 sm:p-8 bg-white relative overflow-x-auto shadow sm:rounded-lg">
-                <a href="{{route('posts.create')}}">
+                <a href="{{ route('posts.create') }}">
                     <button
                         class="bg-transparent float-end hover:bg-green-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                        Add Posts
+                        Add Post
                     </button>
                 </a>
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -50,35 +50,29 @@
                     </thead>
                     <tbody>
                         @foreach ($posts as $item)
-                            <tr
-                                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{$item->title}}
+                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $item->title }}
                                 </th>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{ Str::limit($item->description, 12)}}
-                                </th>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{$item->category->name}}
-                                </th>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{$item->views}}
-                                </th>
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ Str::limit($item->description, 12) }}
+                                </td>
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $item->category->name }}
+                                </td>
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $item->views }}
+                                </td>
                                 <td class="px-6 py-4 text-center">
-                                    <a href="#"
-                                        class=" font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <a href="{{route('posts.show',$item->id)}}"
-                                        class=" ml-5 font-medium text-blue-600 dark:text-blue-500 hover:underline">Show</a>
-                                    <form action="{{ route('posts.destroy', $item->id) }}" method="POST"
-                                        style="display:inline-block;">
+                                    <a href="{{ route('posts.edit', $item->id) }}"
+                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="{{ route('posts.show', $item->id) }}"
+                                        class="ml-5 font-medium text-blue-600 dark:text-blue-500 hover:underline">Show</a>
+                                    <form action="{{ route('posts.destroy', $item->id) }}" method="POST" style="display:inline-block;">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit"
-                                            class=" px-6 font-medium text-red-600 dark:text-red-500 hover:underline"
+                                            class="px-6 font-medium text-red-600 dark:text-red-500 hover:underline"
                                             onclick="return confirm('Are you sure you want to delete this item?')">Delete</button>
                                     </form>
                                 </td>
@@ -86,7 +80,6 @@
                         @endforeach
                     </tbody>
                 </table>
-
             </div>
         </div>
     </div>

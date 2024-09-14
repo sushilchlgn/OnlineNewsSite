8fdf9d844d3b612e50166bb79755c764703c0859
sushil chaulagain <sushilchlgn@gmail.com>
2024-09-14 15:53:40 +0545

    removed

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -10,24 +10,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-6 bg-white shadow sm:rounded-lg">
-                @if ($errors->any())
-                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-                        <strong class="font-bold">Whoops!</strong>
-                        <ul class="mt-3">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                @if (session('success'))
-                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
-                        <strong class="font-bold">Success!</strong>
-                        <span class="block sm:inline">{{ session('success') }}</span>
-                    </div>
-                @endif
-
                 <div class="w-full max-w-lg">
                     <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="POST" class="space-y-6">
                         @csrf
